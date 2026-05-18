8bb9028f36c26b8e2039be5815b1bfc44e9ed804
Michelle D'Andrea <michelledandrea2008@gmail.com>
2026-05-18 21:03:32 +0000

    inizializzazione fork-tps

---
+++ ES02/fork01.c
@@ -0,0 +1,9 @@
+/* fork01.c - © FB - 7/12/20
+ * pid = identificativo del processo in esecuzione */
+#include <stdio.h>   // printf
+#include <unistd.h>  // getpid
+
+int main(int argc, char **argv) {
+
+  printf("\nAvvio del processo con pid=%d\n", getpid());
+}
